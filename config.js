d89d3859c5b82c0e0f3bd82eb9abae798ea318f8
wangzheng <wz@96325>
2020-03-17 15:10:31 +0800

    撤销

---
+++ config.js
@@ -0,0 +1,14 @@
+var config = {
+    // 腾讯云
+    SecretId: 'AKIDSluoSj3fGfx314qSt4Bi6F7k9RUTMlnp',
+    SecretKey: 'rVv99uTmzyiFWZNo0KVTDl36nEahdKrJ',
+    Bucket: '1014-1259043953',
+    Region: 'ap-beijing',
+    // 阿里云
+    region: 'oss-cn-beijing',
+    accessKeyId: 'LTAI4Fd2fU7amH4QLSWz4D5r',
+    accessKeySecret: 'RE5BU7ogcDkVqoco3UzlI4YHbZZZLI',
+    bucket: '1014wang'
+};
+
+module.exports = config;
